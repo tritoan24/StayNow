50ba24278b9e2d988488d2507face50f6a4c7d29
VanQuyet04 <truongquyet137@gmail.com>
2024-12-13 13:53:26 +0700

    pull dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,7 +141,7 @@ dependencies {
     implementation(
         fileTree(
             mapOf(
-                "dir" to "C:\\Do_an_tot_nghiep_StayNow\\StayNow",
+                "dir" to "D:\\KOT104\\Exercises\\StayNow",
                 "include" to listOf("*.aar", "*.jar"),
                 "exclude" to listOf<String>()
             )
